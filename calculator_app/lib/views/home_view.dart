1962d370241d03f41da4e809ec1f2c55b3ca6c2f
m7mdragab <mr341349@gmail.com>
2024-10-10 19:55:42 +0300

    start build ui

--- calculator_app/lib/views/home_view.dart
+++ calculator_app/lib/views/home_view.dart
@@ -5,6 +5,50 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFFFAF6F0),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.topRight,
+                padding: EdgeInsets.all(19),
+                height: 270,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0XFFF4EAE0),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(18),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      '6,291/5',
+                      style: TextStyle(
+                        fontSize: 26,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      '6,2915',
+                      textAlign: TextAlign.end,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
