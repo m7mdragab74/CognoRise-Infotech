5b5a3ee8a6a84290f907d2d37432c081fcca93a9
m7mdragab <mr341349@gmail.com>
2024-10-14 13:43:35 +0300

    done app

--- calculator_app/lib/views/home_view.dart
+++ calculator_app/lib/views/home_view.dart
@@ -1,51 +1,173 @@
+import 'package:calculator_app/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  double firstNum = 0.0;
+  double secondNum = 0.0;
+  String input = "";
+  String output = "";
+  String operation = "";
+  bool hideInput = false;
+  double outputSize = 34.0;
+
+  // Method to handle button clicks
+  void onButtonClick(String value) {
+    if (value == "AC") {
+      input = "";
+      output = "";
+    } else if (value == "<") {
+      if (input.isNotEmpty) {
+        input = input.substring(0, input.length - 1);
+      }
+    } else if (value == "=") {
+      if (input.isNotEmpty) {
+        var userInput = input.replaceAll("x", "*");
+
+        try {
+          Parser p = Parser();
+          Expression expression = p.parse(userInput);
+          ContextModel cm = ContextModel();
+          var finalValue = expression.evaluate(EvaluationType.REAL, cm);
+          output = finalValue.toString();
+          if (output.endsWith(".0")) {
+            output = output.substring(0, output.length - 2);
+          }
+          input = output;
+          hideInput = true;
+          outputSize = 52;
+        } catch (e) {
+          output = "Error";
+        }
+      }
+    } else {
+      input += value;
+      hideInput = false;
+      outputSize = 34;
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFFAF6F0),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.topRight,
-                padding: EdgeInsets.all(19),
-                height: 270,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color(0XFFF4EAE0),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(18),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          // Input and output area
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(
+                    hideInput ? "" : input,
+                    style: const TextStyle(
+                      fontSize: 48,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      '6,291/5',
-                      style: TextStyle(
-                        fontSize: 26,
-                      ),
+                  const SizedBox(height: 20),
+                  Text(
+                    output,
+                    style: TextStyle(
+                      fontSize: outputSize,
+                      color: Colors.white.withOpacity(0.7),
                     ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      '6,2915',
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              )
+                  ),
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ),
+          ),
+          // Buttons area
+          Row(
+            children: [
+              button(
+                  text: "AC",
+                  buttonBgColor: operatorColor,
+                  tColor: orangeColor),
+              button(
+                  text: "<", buttonBgColor: operatorColor, tColor: orangeColor),
+              button(
+                  text: "/", buttonBgColor: operatorColor, tColor: orangeColor),
+              button(
+                  text: "x", buttonBgColor: operatorColor, tColor: orangeColor),
             ],
+          ),
+          Row(
+            children: [
+              button(text: "7"),
+              button(text: "8"),
+              button(text: "9"),
+              button(
+                  text: "-", buttonBgColor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "4"),
+              button(text: "5"),
+              button(text: "6"),
+              button(
+                  text: "+", buttonBgColor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "1"),
+              button(text: "2"),
+              button(text: "3"),
+              button(
+                  text: "%", buttonBgColor: operatorColor, tColor: orangeColor),
+            ],
+          ),
+          Row(
+            children: [
+              button(text: "0"),
+              button(text: "."),
+              button(text: "=", buttonBgColor: orangeColor),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Button widget generator
+  Widget button(
+      {required String text,
+      Color tColor = Colors.white,
+      Color buttonBgColor = buttonColor}) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            backgroundColor: buttonBgColor,
+            padding: const EdgeInsets.all(22),
+          ),
+          onPressed: () => onButtonClick(text),
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 18,
+              color: tColor,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
