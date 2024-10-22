63a57d37d2f981b92084a20553682a8b721daeaa
m7mdragab <mr341349@gmail.com>
2024-10-22 16:55:38 +0300

    create files

--- alarm_app/lib/main.dart
+++ alarm_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alarm_app/home_page.dart';
+import 'package:alarm_app/views/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +10,7 @@ class AlarmApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
