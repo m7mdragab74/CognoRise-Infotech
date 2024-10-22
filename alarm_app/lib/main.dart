adf646128b074a4a009e0ed7662f892af1865a47
m7mdragab <mr341349@gmail.com>
2024-10-22 16:31:09 +0300

    first commit

---
+++ alarm_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:alarm_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AlarmApp());
+}
+
+class AlarmApp extends StatelessWidget {
+  const AlarmApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
