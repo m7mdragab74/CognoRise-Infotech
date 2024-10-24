ad905f40b488dfad251892831d9900dd99785216
m7mdragab <mr341349@gmail.com>
2024-10-24 20:32:20 +0300

    start use local notification and add android primiation

--- alarm_app/lib/main.dart
+++ alarm_app/lib/main.dart
@@ -2,9 +2,37 @@ import 'package:alarm_app/data/enum.dart';
 import 'package:alarm_app/model/menu_info.dart';
 import 'package:alarm_app/views/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var initializationSettingsAndroid =
+      AndroidInitializationSettings('codex_logo');
+
+  var initializationSettingsIOS = DarwinInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  var initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+    onDidReceiveNotificationResponse: (NotificationResponse response) async {
+      if (response.payload != null) {
+        debugPrint('notification payload: ${response.payload}');
+      }
+    },
+  );
+
   runApp(const AlarmApp());
 }
 
