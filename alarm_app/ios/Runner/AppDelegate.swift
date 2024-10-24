d7b5d824152b131738b954458540c57e145a38a5
m7mdragab <mr341349@gmail.com>
2024-10-24 20:41:02 +0300

    integration ios settings

--- alarm_app/ios/Runner/AppDelegate.swift
+++ alarm_app/ios/Runner/AppDelegate.swift
@@ -1,13 +1,20 @@
 import Flutter
 import UIKit
+import UserNotifications // Import UserNotifications for iOS notifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // Set the UNUserNotificationCenter delegate for iOS 10.0+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
